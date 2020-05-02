922f4e70e6d8c148a2229a01a21f5601a9683707
hkdas5464 <hkdas5464@gmail.com>
2020-05-02 17:12:03 +0530

    Updates

---
+++ precache-manifest.c9a05f823b7122d4aca13f8542e56446.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a3343ecb1a860df936b9b066a5e549",
+    "url": "/Mycontact/index.html"
+  },
+  {
+    "revision": "4b97b97cd73e31108122",
+    "url": "/Mycontact/static/css/main.89954aa7.chunk.css"
+  },
+  {
+    "revision": "dd73a8cb1493c6f0932d",
+    "url": "/Mycontact/static/js/2.f62a6928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mycontact/static/js/2.f62a6928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b97b97cd73e31108122",
+    "url": "/Mycontact/static/js/main.93c77dfd.chunk.js"
+  },
+  {
+    "revision": "95ee98f4cf2ac3eabae7",
+    "url": "/Mycontact/static/js/runtime-main.99837085.js"
+  },
+  {
+    "revision": "b4be7ff526452e5578e9400f74b20d5e",
+    "url": "/Mycontact/static/media/abhishek.b4be7ff5.jpg"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/Mycontact/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/Mycontact/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "eb9e558d4cfb459cf3f44ade04dde3f8",
+    "url": "/Mycontact/static/media/hardik.eb9e558d.JPG"
+  },
+  {
+    "revision": "23e67b913f7349350b4f6e11cb89f20c",
+    "url": "/Mycontact/static/media/harendra.23e67b91.jpg"
+  },
+  {
+    "revision": "e288b5286aa60d3cf78bbfa9957adf9b",
+    "url": "/Mycontact/static/media/harendra1.e288b528.jpg"
+  },
+  {
+    "revision": "e4f4c4e45ca3aa7211824215a50dfb88",
+    "url": "/Mycontact/static/media/harendra2.e4f4c4e4.JPG"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/Mycontact/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/Mycontact/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/Mycontact/static/media/search.0fc053ba.svg"
+  }
+]);
